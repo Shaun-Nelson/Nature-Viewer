7c0a8a7069f8e5162198af7add50c41b2fe099cf
Shaun Nelson <shaunptnelson@gmail.com>
2022-12-20 18:55:41 -0500

    Desktop View working

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // import { createClient } from "flickr-sdk";
 const API_KEY = "563492ad6f91700001000001e2f01e5688a94b13b884103cacf2626b";
 const endpoint =
-  "https://api.pexels.com/v1/search/?page=1&per_page=40&query=Nature,animals";
-const search = "/search?query=Nature,animals";
+  "https://api.pexels.com/v1/search/?page=1&per_page=40&query=Nature,animals,jungle,mountains,forest,plants";
+const query = "/search?query=";
+const search = "Nature,animals,jungle,mountains,forest,plants";
 const orient = "&orientation=portait";
 const img = document.getElementById("img");
 const btn = document.getElementById("btn");
@@ -12,7 +13,7 @@ const photographer = document.getElementById("photographer");
 const url = endpoint + search;
 // "curated?per_page=80";
 
-// searchParams.innerHTML = search;
+searchParams.innerHTML = search;
 
 const getPhotos = async (search, handler) => {
   await fetch(search, {
@@ -25,7 +26,6 @@ const getPhotos = async (search, handler) => {
       (rej) => console.log(rej)
     )
     .then((data) => {
-      console.log(data);
       let photos = [];
       for (let photo of data.photos) {
         photos.push(photo.src.portrait);
@@ -35,9 +35,7 @@ const getPhotos = async (search, handler) => {
 };
 
 const displayRandomPhoto = (data) => {
-  console.log(data);
   let randomIndex = Math.floor(Math.random() * data.photos.length);
-  console.log(randomIndex);
   let photo = data.photos[randomIndex];
   img.src = photo.src.portrait;
   photographer.innerHTML = `Photographer: ${photo.photographer}`;
