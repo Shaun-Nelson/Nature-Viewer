b89659c48df3f38274fc1e5439a329c1481b17b4
Shaun Nelson <shaunptnelson@gmail.com>
2022-12-20 23:43:16 -0500

    Styled button

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const API_KEY = "563492ad6f91700001000001e2f01e5688a94b13b884103cacf2626b";
 const endpoint =
   "https://api.pexels.com/v1/search/?page=1&per_page=40&query=Nature";
 const query = "/search?query=";
-const search = "Nature,animals,jungle,mountains,forest,plants";
+const search = "Nature";
 const orient = "&orientation=portait";
 const img = document.getElementById("img");
 const btn = document.getElementById("btn");
 const searchbar = document.getElementById("searchbar");
 const searchParams = document.getElementById("search-parameters");
 const photographer = document.getElementById("photographer");
+const altText = document.getElementById("alt-text");
 const url = endpoint + search;
 // "curated?per_page=80";
 
@@ -26,22 +27,17 @@ const getPhotos = async (search, handler) => {
       (rej) => console.log(rej)
     )
     .then((data) => {
-      let photos = [];
-      for (let photo of data.photos) {
-        photos.push(photo.src.portrait);
-      }
       handler(data);
     });
 };
 
 const displayRandomPhoto = (data) => {
   let photo = data.photos[Math.floor(Math.random() * data.photos.length)];
-  while (window.innerWidth >= 800 && photo.width < 1080) {
-    photo = data.photos[Math.floor(Math.random() * data.photos.length)];
-  }
   window.innerWidth >= 800
-    ? (img.src = photo.src.large2x)
+    ? (img.src = photo.src.original)
     : (img.src = photo.src.portrait);
+  img.alt = photo.alt;
+  altText.innerHTML = img.alt;
   photographer.innerHTML = `Photographer: ${photo.photographer}`;
 };
 
@@ -49,6 +45,5 @@ const updatePhoto = () => {
   getPhotos(endpoint, displayRandomPhoto);
 };
 
-// btn.addEventListener("click", updatePhoto(searchbar.value));
 btn.addEventListener("click", updatePhoto);
 getPhotos(endpoint, displayRandomPhoto);
